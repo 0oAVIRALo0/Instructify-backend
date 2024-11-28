82229c7a4d6cda946d9df7262c594a6382de0d4b
Aviral Chauhan <aviralchauhan@aviral.local>
2024-11-29 02:07:53 +0530

    first commit

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,10 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
-import { DB_NAME } from "../utils/index.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `mongodb+srv://aviralch30:${process.env.MONGO_PASSWORD}@vrv-deployment.6yujg.mongodb.net/?retryWrites=true&w=majority&appName=VRV-Deployment`
     );
     console.log("MongoDB connected");
   } catch (error) {
